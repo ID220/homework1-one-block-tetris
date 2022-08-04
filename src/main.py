062655c572f1fc625bd98200238744030af7fe73
Andrea <andrea@kaist.ac.kr>
2022-08-04 18:45:49 +0900

    WIP

--- src/main.py
+++ src/main.py
@@ -1,1 +1,46 @@
-# TODO
+from pyblinkpico import *
+import time
+
+
+#### TICK HELPER ######
+prevTime = {}
+
+
+def tick(ms, callback):
+    if (ms not in prevTime):
+        prevTime[ms] = 0
+    if time.ticks_diff(time.ticks_ms(), prevTime[ms]) > ms:
+        callback()
+        prevTime[ms] = time.ticks_ms()
+
+
+
+#### MAIN ######
+
+# CONSTANTS
+INPUT_REFRESH = 100
+LOGIC_REFRESH = 500
+
+
+# update this every 500ms
+def updateLogic():
+    # your logic here
+    print('Draw and update your logic')
+    pass
+
+
+# update this every 100ms
+def updateInput():
+    # your logic and draw here
+    print('Read from buttons')
+    pass
+
+
+# main
+while True:
+    # Your code here
+
+    # these two functions set a callback to be called
+    # at respectively 500 and 100 milliseconds
+    tick(INPUT_REFRESH, updateInput)
+    tick(LOGIC_REFRESH, updateLogic)
