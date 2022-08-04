f9fb7fd2e47a091317b16d0b666ad6004926cea9
Andrea <andrea@kaist.ac.kr>
2022-08-04 18:14:35 +0900

    microbit version

--- src/main.py
+++ src/main.py
@@ -1,1 +1,29 @@
-# TODO
+from microbit import *
+# import utime
+from timer import *
+
+# CONSTANTS
+UI_REFRESH = 500
+INPUT_REFRESH = 100
+
+
+# update this every 500ms
+def updatelogic():
+    # your logic here
+    pass
+
+
+# update this every 100ms
+def updateInputUI():
+    # your logic here
+    pass
+
+
+# main
+while True:
+    # your logic here
+
+    # these two functions set a callback to be called
+    # at respectively 500 and 100 milliseconds
+    tick(INPUT_REFRESH, updateInputUI)
+    tick(UI_REFRESH, updatelogic)
